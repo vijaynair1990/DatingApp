c805dc5c7fbfa26e80d5aa4fa8dea908e194a3f0
vijaynair1990 <vjnair6@gmail.com>
2025-03-18 14:18:16 +0530

    observable, signal login persistence

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
+import { AccountService } from './_service/account.service';
 
 @Component({
   selector: 'app-root',
@@ -13,10 +14,24 @@ import { NavComponent } from "./nav/nav.component";
 })
 export class AppComponent implements OnInit {
   http = inject(HttpClient);
+  private accountService=inject(AccountService);
   title = 'DatingApp';
   users: any;
 
   ngOnInit(): void {
+    this.getUsers();
+    this.setCurrentUser();
+  }
+
+  setCurrentUser()
+  {
+    const userString=localStorage.getItem('user');
+    if(!userString) return;
+    const user=JSON.parse(userString);
+    this.accountService.currentUser.set(user);
+  }
+
+  getUsers() {
     this.http.get('http://localhost:5000/api/users').subscribe({
       next: response => this.users = response,
       error: error => console.log(error),
