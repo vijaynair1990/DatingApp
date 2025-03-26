bd4c1cd7bad331a3548ede8a342a479be57dcf71
vijaynair1990 <vjnair6@gmail.com>
2025-03-26 20:03:03 +0530

    Routing, Route guards and theme added

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,12 +3,13 @@ import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
 import { AccountService } from './_service/account.service';
-import { HomeComponent } from "./home/home.component";
+// import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavComponent, HomeComponent],
+  // imports: [RouterOutlet,NavComponent, HomeComponent],
+  imports: [RouterOutlet,NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, input, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_service/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -9,24 +10,23 @@ import { AccountService } from '../_service/account.service';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-  private accountService=inject(AccountService);
+  private accountService = inject(AccountService);
+  private toastr=inject(ToastrService);
 
-cancelRegister=output<boolean>();
-model:any={}
+  cancelRegister = output<boolean>();
+  model: any = {}
 
-register()
-{
-  this.accountService.register(this.model).subscribe({
-    next: response=>{
-      console.log(response);
-      this.cancel();
-    },
-    error:error=>console.log(error)
-  })
-}
+  register() {
+    this.accountService.register(this.model).subscribe({
+      next: response => {
+        console.log(response);
+        this.cancel();
+      },
+      error: error => this.toastr.error(error.error)
+    })
+  }
 
-cancel()
-{
-  this.cancelRegister.emit(false);
-}
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
 }
