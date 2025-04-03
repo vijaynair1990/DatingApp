3c4fc905b7a2a0d575ab73334ed4559bb4cbe18a
vijaynair1990 <vjnair6@gmail.com>
2025-04-03 19:04:55 +0530

    user interface, jwt interceptor, typescript, member details, animated buttons, photo gallery added

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,6 +1,5 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RegisterComponent } from "../register/register.component";
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -8,31 +7,20 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit {
-  http = inject(HttpClient);
-  registerMode = false;
-  users: any;
+export class HomeComponent {
 
-  ngOnInit(): void {
-    this.getUsers();
-  }
+  registerMode = false;
 
 
   registerToggle() {
     this.registerMode = !this.registerMode;
   }
 
-  cancelRegisterMode(event:boolean){
-    this.registerMode=event;
+  cancelRegisterMode(event: boolean) {
+    this.registerMode = event;
   }
 
-  getUsers() {
-    this.http.get('http://localhost:5000/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed')
-    })
-  }
+
 
 }
 
